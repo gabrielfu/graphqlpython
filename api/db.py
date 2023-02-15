34fb04a799f59a62bb4c9297a20b49fb87655949
Gabriel Fu <hfu.gabriel@gmail.com>
2023-02-15 22:09:08 +0800

    remove unnecessary comment

--- api/db.py
+++ api/db.py
@@ -17,9 +17,6 @@ Base = declarative_base()
 Base.query = db_session.query_property()
 
 def init_db():
-    # import all modules here that might define models so that
-    # they will be registered properly on the metadata.  Otherwise
-    # you will have to import them first before calling init_db()
     from models import ActorModel, MovieModel
 
     Base.metadata.drop_all(bind=engine)
