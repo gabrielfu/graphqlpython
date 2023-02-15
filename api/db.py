61f439086ab607b545d4b2c592c310e8d44ae77e
Gabriel Fu <hfu.gabriel@gmail.com>
2023-02-15 22:56:59 +0800

    remove pandas & parquet dependency

--- api/db.py
+++ api/db.py
@@ -1,13 +1,13 @@
 import os
-import pandas as pd
+import csv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 POSTGRES_DB_URI = os.environ.get("POSTGRES_DB_URI", "postgresql://user:123456@localhost:5432/imdb_db")
-FIXTURE_MOVIES_PATH = os.environ.get("FIXTURE_MOVIES_PATH", "../data/imdb/movies.parquet")
-FIXTURE_ACTORS_PATH = os.environ.get("FIXTURE_ACTORS_PATH", "../data/imdb/actors.parquet")
-FIXTURE_ASSOCIATION_PATH = os.environ.get("FIXTURE_ASSOCIATION_PATH", "../data/imdb/actor_movie.parquet")
+FIXTURE_MOVIES_PATH = os.environ.get("FIXTURE_MOVIES_PATH", "../data/imdb/movies.csv")
+FIXTURE_ACTORS_PATH = os.environ.get("FIXTURE_ACTORS_PATH", "../data/imdb/actors.csv")
+FIXTURE_ASSOCIATION_PATH = os.environ.get("FIXTURE_ASSOCIATION_PATH", "../data/imdb/actor_movie.csv")
 
 engine = create_engine(POSTGRES_DB_URI, convert_unicode=True)
 db_session = scoped_session(
@@ -23,17 +23,23 @@ def init_db():
     Base.metadata.create_all(bind=engine)
 
     actors = {}
-    for _, row in pd.read_parquet(FIXTURE_ACTORS_PATH).iterrows():
-        item = ActorModel(**row.to_dict())
-        actors[item.id] = item
+    with open(FIXTURE_ACTORS_PATH, newline='') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            item = ActorModel(**row)
+            actors[item.id] = item
 
     movies = {}
-    for _, row in pd.read_parquet(FIXTURE_MOVIES_PATH).iterrows():
-        item = MovieModel(**row.to_dict())
-        movies[item.id] = item
+    with open(FIXTURE_MOVIES_PATH, newline='') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            item = MovieModel(**row)
+            movies[item.id] = item
 
-    for _, (movie_id, actor_id) in pd.read_parquet(FIXTURE_ASSOCIATION_PATH).iterrows():
-        movies[movie_id].actors.append(actors[actor_id])
+    with open(FIXTURE_ASSOCIATION_PATH, newline='') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            movies[row["movie_id"]].actors.append(actors[row["actor_id"]])
 
     for item in actors.values():
         db_session.add(item)
